334d60dd5b3680d319810fa4393c8823fa863af6
TruongTheHieu <Thehieu191099@gmail.com>
2023-10-25 18:25:31 +0700

    nop bai

---
+++ modue2/src/ss1_modue2/HienThiLoiChao.java
@@ -0,0 +1,12 @@
+package ss1_modue2;
+
+import java.util.Scanner;
+
+public class HienThiLoiChao {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name ");
+        String name = scanner.nextLine();
+        System.out.println("hello "+ name);
+    }
+}
