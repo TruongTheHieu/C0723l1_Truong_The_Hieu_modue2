968b4212f293459c5fe49c7a1242729453dcdc17
TruongTheHieu <thehieu191099@gmail.com>
2023-11-24 13:05:43 +0700

    up date

--- module2/src/ss11_dsa_stack_queue/stack/ReverseByUsingStack.java
+++ module2/src/ss11_dsa_stack_queue/stack/ReverseByUsingStack.java
@@ -9,17 +9,18 @@ public class ReverseByUsingStack {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Stack<Integer> arrayInt = new Stack<>();
-        System.out.println("nhập vào mảng số có 5 phần tử");
-        for (int i = 0; i <5 ; i++) {
-            int n = Integer.parseInt(sc.nextLine());
+        System.out.println("nhập vào mảng số 10 phần tử");
+        for (int i = 0; i <10; i++) {
+            int n = sc.nextInt();
             arrayInt.push(n);
         }System.out.println("mảng ban đầu là :" + arrayInt);
         System.out.println("mảng sau khi đảo");
-        for (int i = 0; i <arrayInt.size() ; i++) {
-            System.out.println(arrayInt.pop());
+        for (int i = 0; !arrayInt.isEmpty() ; i++) {
+            System.out.print(arrayInt.pop());
+        }
         }
 
     }
-}
+
 
 
